7f11224528df8d2d3c2af13fd83711b15cfcf2f9
JX <jingxian.lau01@gmail.com>
2022-06-18 11:30:27 +0800

    Initial Commit

---
+++ Namecard/NamecardApp.swift
@@ -0,0 +1,17 @@
+//
+//  NamecardApp.swift
+//  Namecard
+//
+//  Created by TinkerTanker on 18/6/22.
+//
+
+import SwiftUI
+
+@main
+struct NamecardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
